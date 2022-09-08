1b6f46f24a7e4886186755c971e164d930b7881f
Roger Caballero <rcaballero@ssn.gob.ar>
2022-09-08 09:38:13 -0300

    carga de lista e imagenes

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import ItemCount from '../../components/ItemCount';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import ItemList from '../../components/ItemList';
+//import ItemCount from '../../components/ItemCount';
+import { products } from '../../data/products';
 import './styles.css';
 
 const ItemListContainer = ({greeting}) => {
+// hook useState array en 0
+    const [productos,setProductos] = useState([])
+//la promise se ejecuta x unica vechi
+    useEffect (()=>{
+        ( async ()=> {
+            const obtenerProductos = new Promise ((accept, reject) => { 
+                setTimeout (()=> {
+                    accept(products)
+                }, 3000); 
+                //pasa 3 segundo y se renderiza
+            })
+            //promesa
+            try {
+        const response = await obtenerProductos;
+        //console.log(response);
+        setProductos(response);
+    } catch (error) {
+        console.log(error);
+        }
+        })()
+    }, [productos])
+
+
+    console.log(productos);
 return (
-<div>
-<h1>{greeting}</h1>
-    <ItemCount/>
+
+<div className='item-list-container'>
+    <ItemList products={productos}/>
+
 </div>
 
 )
---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+export const products = [
+{id: 1, description: "Descripción 1", name: "Roger 1", stock:10,},
+{id: 2, description: "Descripción 2", name: "Roger 2", stock:8,},
+{id: 3, description: "Descripción 3", name: "Roger 3", stock:4,},
+{id: 4, description: "Descripción 4", name: "Roger 4", stock:15,},
+
+];
