5307973b8fde12699764385ab411bf634fa47e26
Roger Caballero <rcaballero@ssn.gob.ar>
2022-09-08 10:00:32 -0300

    cambio en traer listado y card

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './styles.css';
+import './styles.css'
 import logo from '../../img/man.jpg';
 
 const Item = ({product}) => {
   return (
    //<h1>{product.name}</h1>
    <div class="card">
-    <img src={logo} alt="logo" />  
+    <img src={logo} alt="logo"  />  
       <div class="container">
      <h4><b>{product.name}</b></h4>
      <p>Super Cards</p>
