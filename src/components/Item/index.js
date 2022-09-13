fe897e8f5ccd54cc2d28850e3672206d21fed9ce
Roger Caballero <rcaballero@ssn.gob.ar>
2022-09-13 10:03:31 -0300

    cambio de css

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -6,17 +6,23 @@ import './styles.css'
 const Item = ({product}) => {
   return (
    //<h1>{product.name}</h1>
-
-     <div class="card">
-    <img src={product.image} width= {200} alt="logo"  />  
-      <div class="container">
-     <h4><b>{product.title}</b></h4>
-     <p>{product.description}</p>
-     <h3><b>${product.price }</b></h3>
-   </div>
+     /*<div class="card">
+      <img src={product.image} width= {200} alt="logo"  />  
+       <div class="container">
+        <h4><b>{product.title}</b></h4>
+          <p>{product.description}</p>
+      <h3><b>${product.price }</b></h3>
+      </div>
+    </div>*/
+//--------------------
+<div class="card">
+  <img class="card-img-top" src={product.image} width= {250}  alt="Ver"/>
+  <div class="card-body">
+    <h5 class="card-title">{product.title}</h5>
+    <p class="card-text">${product.description}</p>
+    <a class="btn btn-primary">Comprar</a>
+  </div>
 </div>
-
-  
   )
 }
 
